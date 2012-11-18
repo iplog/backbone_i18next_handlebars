93739bc156deb08cb5c7934f0d7a3b58aec694ce
Nicolas Blanchot <nicolasblanchot@gmail.com>
2012-11-19 00:27:52 +0100

    fix typo in app/utils/helpers.js

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -5,14 +5,14 @@ define([
   i18n
 ) {
   var helpers = {
-    t: function(i18n_key) {
-      var result = i18n.t(i18n_key);
+    t: function(i18nKey) {
+      var result = i18n.t(i18nKey);
       return new Handlebars.SafeString(result);
     },
     tr: function(context, options) {
       var opts = i18n.functions.extend(options.hash, context);
       if (options.fn) opts.defaultValue = options.fn(context);
-      var result = i18next.t(opts.key, opts);
+      var result = i18n.t(opts.key, opts);
       return new Handlebars.SafeString(result);
     }
   };
